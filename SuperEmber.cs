88e8926429fc37ac3b0ee52b66bd8c10f614ae2e
frjabur <frjabur@gmail.com>
2017-06-20 10:29:46 -0300

    Update SuperEmber.cs

--- SuperEmber.cs
+++ SuperEmber.cs
@@ -192,7 +192,8 @@
                             }
                         }
                     }
-                
+                }
+		}
 //test finish
         public async Task UseItems(Unit target, CancellationToken token)
         {
